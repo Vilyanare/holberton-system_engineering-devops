5467b2929c2b59b898376393f40beb1cf2d8de66
vilyanare <acolyteofwar@gmail.com>
2018-05-24 17:47:53 -0700

    Now lint compliant

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,8 @@
 # Replaces the broken phpp string with php in /var/www/html/wp-settings.php
 file { '/var/www/html/wp-settings.php':
-  ensure => present,
+  ensure   => present,
 }->
 exec { 'sed command to replace phpp with php':
-  command => "sed -i -e 's/phpp/php/g' /var/www/html/wp-settings.php",
-  path => '/bin',
+  command  => 'sed -i -e "s/phpp/php/g" /var/www/html/wp-settings.php',
+  path     => '/bin',
 }
